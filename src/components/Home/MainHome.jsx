d45da40a49b12650726371d46aa578cbdfcfbff1
Thomas Dejardins <tdejardins@gmail.com>
2022-10-13 17:00:42 +0200

    feat(MainHome.jsx): add MainHome component

---
+++ src/components/Home/MainHome.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import Features from "./Features";
+
+function MainHome() {
+  return (
+    <main>
+      <Banner />
+      <Features />
+    </main>
+  );
+}
+
+export default MainHome;

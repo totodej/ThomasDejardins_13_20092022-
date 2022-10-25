577f9477615e90e7b40ccad68f4ffd149ba96870
Thomas Dejardins <tdejardins@gmail.com>
2022-10-25 18:51:32 +0200

    feat(User.jsx): add the connection with the store

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,29 +1,24 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MainUser from "../components/User/MainUser";
-import userProfile from "../services/userProfile";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 function User() {
-  const token = localStorage.getItem("token");
-  const [data, setData] = useState(null);
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const getData = async () => {
-      const result = await userProfile(token);
-      setData(result);
-    };
-    getData();
-  }, [token]);
-
-  if (!data) {
-    return null;
-  }
+    if (!auth.token) {
+      navigate("/");
+    }
+  }, [auth.token, navigate]);
 
   return (
     <div>
       <Header />
-      <MainUser dataUserInfo={data.body} />
+      <MainUser />
       <Footer />
     </div>
   );

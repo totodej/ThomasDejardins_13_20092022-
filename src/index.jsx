8818ff975edb082d9c33183c3cede15d25e6ecc4
Thomas Dejardins <tdejardins@gmail.com>
2022-10-25 18:28:14 +0200

    feat(index.jsx): add provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import User from "./pages/User";
+import store from "./utils/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/profile" element={<User />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/profile" element={<User />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
